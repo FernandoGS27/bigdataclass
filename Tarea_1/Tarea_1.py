264ee5149eb03a1aa9757d07c27ce8983fa25685
FernandoGS27 <luisferguti27@gmail.com>
2024-04-06 16:40:55 -0600

    Update Tarea_1.py

--- Tarea_1/Tarea_1.py
+++ Tarea_1/Tarea_1.py
@@ -50,5 +50,5 @@ df_joined_1.show()
 
 #Ahora se hace un left join entre el primer join y curso para obtener los creditos
 
-df_joined_2=df_joined_1.join(curso_df,on='Codigo de Curso',how='left',suffixes('_left','_right'))
+df_joined_2=df_joined_1.join(curso_df,on='Codigo de Curso',how='left',suffixes=('_left','_right'))
 df_joined_2.summary().show()
