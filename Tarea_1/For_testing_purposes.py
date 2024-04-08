92492493ef2f19d5c0187e1424eb8d84f47bf04f
FernandoGS27 <luisferguti27@gmail.com>
2024-04-07 22:32:50 -0600

    Create For_testing_purposes.py

---
+++ Tarea_1/For_testing_purposes.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, date_format, udf
+from pyspark.sql.types import (DateType, IntegerType, FloatType, StringType,
+                               StructField, StructType, TimestampType)
+
+spark = SparkSession.builder.appName("Read Transactions").getOrCreate()
+
+expected_ds = spark_session.createDataFrame ([(980,1,70.5,'John Lennon','Computacion',3),(325,1,85.4,'John Lennon','Computacion'),(980,2,55.4,'Paul McCartney','Computacion',3),\
+                                                  (725,3,95.5,'Ringo Starr', 'Computacion',4),(589,4,45.4,'George Harrison' 'Fisica',2),(589,4,89.3,'George Harrison','Fisica',2)],\
+                                                  ['Codigo de Curso','Numero de Carnet','Nota','Nombre Completo','Carrera','Credito'])
+expected_ds.show()
