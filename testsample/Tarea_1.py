d98ddfc72dd955cfdcaf3ed1c2099775b63a5295
FernandoGS27 <luisferguti27@gmail.com>
2024-04-07 21:45:06 -0600

    Cambio de Carpeta

---
+++ testsample/Tarea_1.py
@@ -0,0 +1,155 @@
+from datetime import datetime
+
+from pyspark.sql import SparkSession
+from pyspark.sql import Window
+from pyspark.sql.functions import col, date_format, udf, rank
+from pyspark.sql.types import (DateType, IntegerType, FloatType, StringType,
+                               StructField, StructType, TimestampType)
+
+spark = SparkSession.builder.appName("Tarea_1").getOrCreate()
+
+estudiantes_schema = StructType([StructField('Numero de Carnet',IntegerType()),
+                                StructField('Nombre Completo',StringType()),
+                                StructField('Carrera',StringType())])
+                                
+
+estudiantes_df = spark.read.csv('estudiante.csv',
+                                    schema=estudiantes_schema,
+                                    header=False)
+                                    
+estudiantes_df.printSchema()
+estudiantes_df.show()
+                                    
+curso_schema = StructType([StructField('Codigo de Curso',IntegerType()),
+                                StructField('Credito',IntegerType()),
+                                StructField('Carrera_c',StringType())])
+                                
+curso_df = spark.read.csv('curso.csv',
+                                    schema=curso_schema,
+                                    header=False)
+curso_df.printSchema()
+curso_df.show()
+                                    
+nota_schema = StructType([StructField('Numero de Carnet',IntegerType()),
+                                StructField('Codigo de Curso',IntegerType()),
+                                StructField('Nota',FloatType())])
+                                
+nota_df = spark.read.csv('nota.csv',
+                                    schema=nota_schema,
+                                    header=False)
+                                    
+nota_df.printSchema()
+nota_df.show()
+
+##Joins##
+
+##Primero se hace un left join entre Nota y Estudiante. Esto implica que los estudiantes que aparecen el df 'estudiante' pero no no matricularan curso dentro del periodo de referencia no aparecen en el df 'Nota' y por tanto no son considerados
+
+# df_joined_1= nota_df.join(estudiantes_df,on='Numero de Carnet', how='left')
+# #df_joined_1.summary().show()
+# df_joined_1.show()
+
+# #Ahora se hace un left join entre el primer join y curso para obtener los creditos
+
+# df_joined_2=df_joined_1.join(curso_df,on='Codigo de Curso',how='left')
+# #df_joined_2.summary().show()
+# df_joined_2.show()
+
+def unir_datos(nota,estudiantes,curso):
+    '''
+    La funcion recibe Tres dataframes y devuelve la union de los 3.
+    Se asume que los estudiantes listados en el Dataset 'estudiantes_df' pero no en el dataset 'nota_df' no matricularon cursos
+    en el periodo de referencia y por ende no se toman en cuenta para la obtencion de los mejores promedios.
+    
+    Args:
+    nota: dataframe
+    estudiante: dataframe
+    curso: dataframe
+    
+    Return:
+    segundo_join_df: La union de los tres dataframes  
+    '''
+    
+    primer_join_df = nota_df.join(estudiantes_df,on = 'Numero de Carnet', how = 'left')
+    segundo_join_df = primer_join_df.join(curso_df,on = 'Codigo de Curso',how = 'left').drop('Carrera_c')
+
+    return segundo_join_df
+    
+df_joined_2 = unir_datos(nota_df,estudiantes_df,curso_df)
+df_joined_2.show()
+
+
+
+# nota_ponderada_df = df_joined_2.withColumn('nota_ponderada', col('Nota') * col('Credito')).drop('Carrera_c','Codigo de Curso','Nota','Numero de Carnet')
+# nota_ponderada_df.show()
+# nota_ponderada_df.printSchema()
+
+# agrupar_por_estudiante_df = nota_ponderada_df.groupBy("Nombre Completo", "Carrera").sum()
+# agrupar_por_estudiante_df.show()
+
+# agrupar_por_estudiante_sumas_df = \
+    # agrupar_por_estudiante_df.select(
+        # col('Nombre Completo'),
+        # col('Carrera'),
+        # col('sum(Credito)').alias('Credito'),col('sum(nota_ponderada)').alias('nota_ponderada'))
+# agrupar_por_estudiante_sumas_df.show()
+
+# promedio_poderado_df=agrupar_por_estudiante_sumas_df.withColumn("promedio_ponderado", col('nota_ponderada') / col('Credito')).drop('Credito', 'nota_ponderada')
+# promedio_poderado_df.show()
+
+def agregaciones_parciales(df):
+    '''La funcion recibe un dataframe creado por la funcion 'unir datos' y devuelve un nuevo dataframe que contiene datos correspondientes a 
+    Nombre Completo, Carrera y Promedio Ponderado para cada estudiante
+    
+    Args:
+    df: dataframe
+    
+    Return:
+    
+    promedio ponderado: dataframe
+    '''
+    nota_ponderada = df.withColumn('nota_ponderada', col('Nota') * col('Credito')).drop('Carrera_c','Codigo de Curso','Nota','Numero de Carnet')
+    agrupar_por_estudiante= nota_ponderada.groupBy("Nombre Completo", "Carrera").sum()
+    agrupar_por_estudiante_sumas = \
+    agrupar_por_estudiante.select(
+        col('Nombre Completo'),
+        col('Carrera'),
+        col('sum(Credito)').alias('Credito'),col('sum(nota_ponderada)').alias('nota_ponderada'))
+    promedio_ponderado = agrupar_por_estudiante_sumas.withColumn("promedio_ponderado", col('nota_ponderada') / col('Credito')).drop('Credito', 'nota_ponderada')
+   
+    return promedio_ponderado
+    
+promedio_ponderado_df = agregaciones_parciales(df_joined_2)
+promedio_ponderado_df.show()
+    
+
+    
+
+# particion_carrera_df = Window.partitionBy("Carrera").orderBy(col("promedio_ponderado").desc())
+
+# rankin_df = promedio_ponderado_df.withColumn("rank",rank().over(particion_carrera_df))
+
+# mejores_dos_promedios_carrera = rankin_df.filter(col("rank") <= 2).drop("rank")
+
+# mejores_dos_promedios_carrera.show()
+
+
+def resultados_finales(df):
+    '''La funcion recibe el dataframe generado en la funcion agregaciones parciales y devuelve los dos mejores promedios por cada carrera
+    Args:
+    df: dataframe
+    
+    Return:
+    
+    mejores_dos_promedios_carrera: dataframe
+    
+    '''
+    particion_carrera = Window.partitionBy("Carrera").orderBy(col("promedio_ponderado").desc())
+    rankin_df = df.withColumn("rank",rank().over(particion_carrera))
+    mejores_dos_promedios_carrera = rankin_df.filter(col("rank") <= 2).drop("rank")
+    mejores_dos_promedios_carrera_renombrada = mejores_dos_promedios_carrera.withColumnRenamed('promedio_ponderado','Mejores_promedios')
+    
+    return mejores_dos_promedios_carrera_renombrada
+    
+mejores_dos_promedios_carrera_df = resultados_finales(promedio_ponderado_df)
+mejores_dos_promedios_carrera_df.show()
---
+++ testsample/test_Tarea_1.py
@@ -0,0 +1,23 @@
+from .Tarea_1 import unir_datos
+
+def test_unir_datos(spark_session):
+    nota_data = [(1,980,70.5), (1,325,85.4),(2,980,55.4),(3,725,95.5), (4,589,45.4), (4,589,89.3)]
+    nota_ds = spark_session.createDataFrame(nota_data, ['Numero de Carnet','Codigo de Curso','Nota'])
+    
+    estudiantes_data = [(1,'John Lennon', 'Computacion'),(2,'Paul McCartney', 'Computacion'), (3, 'Ringo Starr', 'Computacion') (4,'George Harrison','Fisica'),(5,'Pete Best','Fisica')]
+    estudiantes_ds = spark_session.createDataFrame(estudiantes_data, ['Numero de Carnet','Nombre Completo','Carrera'])
+    
+    curso_data = [(980,3,'Computacion'),(325,2,'Computacion'),(589,2,'Fisica'),(725,4,'Computacion')]
+    curso_ds = spark_session.createDataFrame(curso_data,['Codigo de Curso','Credito','Carrera_c'])
+    
+    actual_ds = unir_datos(nota_ds,estudiantes_ds,curso_ds)
+    
+    expected_ds = spark_session.createDataFrame ([(980,1,70.5,'John Lennon','Computacion',3),(325,1,85.4,'John Lennon','Computacion'),(980,2,55.4,'Paul McCartney','Computacion',3)\
+                                                  (725,3,95.5,'Ringo Starr', 'Computacion',4),(589,4,45.4,'George Harrison' 'Fisica',2),(589,4,89.3,'George Harrison','Fisica',2)],\
+                                                  ['Codigo de Curso','Numero de Carnet','Nota','Nombre Completo','Carrera','Credito'])
+    expected_ds.show()
+    actual_ds.show()
+    
+    assert actual_ds.collect() == expected_ds.collect()
+                    
+
