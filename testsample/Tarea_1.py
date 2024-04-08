ca3d4e3bdd53c04daa9ce3d484b5370f1bca626c
FernandoGS27 <luisferguti27@gmail.com>
2024-04-07 23:35:31 -0600

    Update Tarea_1.py

--- testsample/Tarea_1.py
+++ testsample/Tarea_1.py
@@ -12,7 +12,7 @@ estudiantes_schema = structtype([structfield('numero de carnet',integertype()),
                                 structfield('nombre completo',stringtype()),
                                 structfield('carrera',stringtype())])
                                 
- estudiantes_df = spark.read.csv('estudiante.csv',
+estudiantes_df = spark.read.csv('estudiante.csv',
                                     schema=estudiantes_schema,
                                      header=false)
                                     
