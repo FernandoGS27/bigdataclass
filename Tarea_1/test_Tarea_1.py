aafd1219694fe2521f751193a7ea97e21e559ce5
FernandoGS27 <luisferguti27@gmail.com>
2024-04-07 20:58:08 -0600

    Update test_Tarea_1.py

--- Tarea_1/test_Tarea_1.py
+++ Tarea_1/test_Tarea_1.py
@@ -4,11 +4,11 @@ def test_unir_datos(spark_session):
     nota_data = [(1,980,70.5), (1,325,85.4),(2,980,55.4),(3,725,95.5), (4,589,45.4), (4,589,89.3)]
     nota_ds = spark_session.createDataFrame(nota_data, 'Numero de Carnet','Codigo de Curso','Nota')
     
-    curso_data = [(980,3,'Computacion'),(325,2,'Computacion'),(589,2,'Fisica'),(725,4,'Computacion')]
-    curso_ds = spark_session.createDataFrame(curso_data,['Codigo de Curso','Credito','Carrera_c'])
-    
     estudiantes_data = [(1,'John Lennon', 'Computacion'),(2,'Paul McCartney', 'Computacion'), (3, 'Ringo Starr', 'Computacion') (4,'George Harrison','Fisica'),(5,'Pete Best','Fisica')]
     estudiantes_ds = spark_session.createDataFrame(estudiantes_data, ['Numero de Carnet','Nombre Completo','Carrera']
+    
+    curso_data = [(980,3,'Computacion'),(325,2,'Computacion'),(589,2,'Fisica'),(725,4,'Computacion')]
+    curso_ds = spark_session.createDataFrame(curso_data,['Codigo de Curso','Credito','Carrera_c'])
     
     actual_ds = unir_datos(nota_ds,estudiantes_ds,curso_ds)
     
