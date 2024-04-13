32959fb795539738abb66191199f018114bfec21
FernandoGS27 <luisferguti27@gmail.com>
2024-04-13 17:53:51 -0600

    Update Tarea_2.py

--- Tarea_2/Tarea_2.py
+++ Tarea_2/Tarea_2.py
@@ -4,13 +4,13 @@ from pyspark.sql.types import (DateType, IntegerType, FloatType, StringType,
 
 spark = SparkSession.builder.appName("Tarea_2").getOrCreate()
 
-schema = StructType([
-    StructField("nombre", StringType(), True),
-    StructField("cantidad", IntegerType(), True),
-    StructField("precio_unitario", FloatType(), True)
-])
+# schema = StructType([
+#     StructField("nombre", StringType(), True),
+#     StructField("cantidad", IntegerType(), True),
+#     StructField("precio_unitario", FloatType(), True)
+# ])
 
-df = spark.read.option("multiline","true").json("compras_1.json",schema=schema)
+df = spark.read.option("multiline","true").json("compras_1.json")
 
 # Show the DataFrame schema
 df.printSchema()
