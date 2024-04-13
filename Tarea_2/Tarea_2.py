5599c4f4ca476144f4065a448f4669874a9628b9
FernandoGS27 <luisferguti27@gmail.com>
2024-04-13 17:32:40 -0600

    Update Tarea_2.py

--- Tarea_2/Tarea_2.py
+++ Tarea_2/Tarea_2.py
@@ -2,10 +2,10 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("Tarea_2").getOrCreate()
 
-df = spark.read.json("compras_1.json")
+df = spark.read.option("multiline","true").json("compras_1.json")
 
 # Show the DataFrame schema
 df.printSchema()
 
 # Show the first few rows of the DataFrame
-df.show()
+df.show(truncate=False)
