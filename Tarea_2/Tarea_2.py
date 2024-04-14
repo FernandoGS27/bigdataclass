b94dab3ccd77c4bdd176738c55428ae7d1693548
FernandoGS27 <luisferguti27@gmail.com>
2024-04-13 18:12:29 -0600

    Update Tarea_2.py

--- Tarea_2/Tarea_2.py
+++ Tarea_2/Tarea_2.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import (DateType, IntegerType, FloatType, StringType,
                                StructField, StructType, TimestampType)
+from pyspark.sql.functions import explode
 
 spark = SparkSession.builder.appName("Tarea_2").getOrCreate()
 
@@ -11,6 +12,16 @@ spark = SparkSession.builder.appName("Tarea_2").getOrCreate()
 # ])
 
 df = spark.read.option("multiline","true").json("compras_1.json")
+
+df_exploded = df.select(explode("compras").alias("compra"))
+
+# Select columns "nombre", "cantidad", and "precio_unitario"
+df_final = df_exploded.select(
+    df_exploded["compra.nombre"].alias("nombre"),
+    df_exploded["compra.cantidad"].alias("cantidad"),
+    df_exploded["compra.precio_unitario"].alias("precio_unitario")
+)
+
 
 # Show the DataFrame schema
 df.printSchema()
