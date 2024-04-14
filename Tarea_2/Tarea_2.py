e86e76c028451b5dc5959586ff5b03f098994733
FernandoGS27 <luisferguti27@gmail.com>
2024-04-13 19:16:59 -0600

    Update Tarea_2.py

--- Tarea_2/Tarea_2.py
+++ Tarea_2/Tarea_2.py
@@ -28,9 +28,9 @@ df.printSchema()
 df_final.printSchema()
 
 
-df_exploded_2 = df_final.withColumn("new", arrays_zip("nombre", "cantidad","precio_unitario"))\
-.withColumn("new", explode("new"))\
-.select(col("new.nombre").alias("Nombre"), col("new.cantidad").alias("Cantidad"),col("new.precio_unitario").alias("Precio_Unitario"))
+df_exploded_2 = df_final.withColumn("nueva", arrays_zip("nombre", "cantidad","precio_unitario"))\
+.withColumn("nueva", explode("nueva"))\
+.select(col("nueva.nombre").alias("Nombre"), col("nueva.cantidad").alias("Cantidad"),col("nueva.precio_unitario").alias("Precio_Unitario"))
 
 df_exploded_2.printSchema()
 df_exploded_2.show()
