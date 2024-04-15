2c7f708418b40cea200b26739a6d1672b4a248a8
FernandoGS27 <luisferguti27@gmail.com>
2024-04-15 12:39:25 -0600

    Update Tarea_2.py

--- Tarea_2/Tarea_2.py
+++ Tarea_2/Tarea_2.py
@@ -85,7 +85,7 @@ def calcular_metricas(df_jsons,df_ventas,df_producto):
     return df_metricas
 
 metricas = calcular_metricas(dataframes_jsons,total_vendido,productos)
-metricas_test= metricas.write.csv("Tarea_2/metricas.csv",header=True,mode="overwrite")
+metricas_test= metricas.write.csv("metricas.csv",header=True,mode="overwrite")
 #metricas.show()
 
 
