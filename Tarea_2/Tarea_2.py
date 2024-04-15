365aaa5e12818519a4e2142e69e88cf354722c4b
FernandoGS27 <luisferguti27@gmail.com>
2024-04-15 12:14:49 -0600

    Update Tarea_2.py

--- Tarea_2/Tarea_2.py
+++ Tarea_2/Tarea_2.py
@@ -85,7 +85,7 @@ def calcular_metricas(df_jsons,df_ventas,df_producto):
     return df_metricas, df_metricas_csv
 
 metricas = calcular_metricas(dataframes_jsons,total_vendido,productos)
-metricas.show()
+#metricas.show()
 
 
 
