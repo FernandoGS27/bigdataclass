54ff35a55ddb599b3e029d9d8d4aba100f24635b
FernandoGS27 <luisferguti27@gmail.com>
2024-04-17 15:40:41 -0600

    Update test_Tarea_2.py

--- Tarea_2/test_Tarea_2.py
+++ Tarea_2/test_Tarea_2.py
@@ -31,18 +31,18 @@ def test_total_cajas(spark_session):
 
 def test_calcular_metricas(spark_session):
 
-    cajas_data = [(1,"A",4,100.0), (1,"B",3,200.0),(1,"C",2,200.0),(2,"D",1,1000.0), (2,"E",3,700.0), (3,"A",1,100.0),(3,"C",4,200.0),(3,"D",6,1000.0),(3,"E",3,700.0),(4,"B",2,300.0),(4,"C",4,200.0)]
+    cajas_data = [(1,"A",4,100.0), (1,"B",3,200.0),(1,"C",2,200.0),(2,"D",1,1000.0), (2,"E",3,700.0), (3,"A",1,100.0),(3,"C",4,200.0),(3,"D",6,1000.0),(3,"E",3,700.0),(4,"B",2,300.0),(4,"C",4,300.0)]
     cajas_ds = spark_session.createDataFrame(cajas_data, ['numero_caja','Nombre','Cantidad','Precio_Unitario'])
 
     total_producto_data = [('A',5),('B',5),('D',7),('C',10),('E',6)]
     total_producto_ds = spark_session.createDataFrame(total_producto_data, ['Nombre','Cantidad_Total'])
 
-    total_ventas_data = [(1,500.0),(2,1700.0),(3,2000.0),(4,500.0)]
+    total_ventas_data = [(1,500.0),(2,1700.0),(3,2000.0),(4,600.0)]
     total_ventas_ds = spark_session.createDataFrame(total_ventas_data,['numero_caja','Total_Vendido'])
 
     actual_ds = calcular_metricas(cajas_ds,total_ventas_ds,total_producto_ds)
 
-    expected_ds = spark_session.createDataFrame([("caja_con_mas_ventas","3"),("caja_con_menos_ventas","4"),("percentil_25_por_caja","500.0"),("percentil_50_por_caja","500.0"),
+    expected_ds = spark_session.createDataFrame([("caja_con_mas_ventas","3"),("caja_con_menos_ventas","1"),("percentil_25_por_caja","500.0"),("percentil_50_por_caja","600.0"),
                                                 ("percentil_75_por_caja","1700.0"),("producto_mas_vendido_por_unidad","C"),
                                                     ("producto_de_mayor_ingreso","D")],['Tipo_de_Metrica','Valor'])
 
